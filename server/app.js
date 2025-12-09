1db6b8ba5486d78c03004ed5f6dfec7a28394f15
Vishwanath20 <visupater@gmail.com>
2025-12-09 14:25:52 +0530

    ff

--- server/app.js
+++ server/app.js
@@ -2,15 +2,24 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import analyzeResumeRoute  from "./routes/analyzeResume.js";
+import analyzeResumeRoute from "./routes/analyzeResume.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
+// Core Middleware
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/resume", analyzeResumeRoute );
+// API Routes
+app.use("/api/resume", analyzeResumeRoute);
+app.use("/api/users", userRoutes);
+
+// Sample root route
+app.get("/", (req, res) => {
+  res.send("Server is working with MongoDB 🎉");
+});
 
 export default app;
