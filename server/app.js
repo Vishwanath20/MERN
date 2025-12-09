e566105b9c4f2f511670e92e817b00d174cd72f8
Vishwanath20 <visupater@gmail.com>
2025-12-09 14:21:50 +0530

    hh

--- server/app.js
+++ server/app.js
@@ -1,21 +1,16 @@
 // server/app.js
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import analyzeResumeRoute  from "./routes/analyzeResume.js";
-import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// API Routes
 app.use("/api/resume", analyzeResumeRoute );
-app.use('/api/users', userRoutes);
-
-// Sample root route
-app.get("/", (req, res) => {
-  res.send("Server is working with MongoDB 🎉");
-});
 
 export default app;
--- server/index.js
+++ server/index.js
@@ -1,17 +1,42 @@
 // server/index.js
+//import express from "express";
+import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import app from "./app.js";
 
 dotenv.config();
-
 const PORT = process.env.PORT || 5000;
+
+//const app = express();
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 // MongoDB connection
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("DB Error:", err));
+
+  // Sample API route
+  app.get("/", (req, res) => {
+    res.send("Server is working with MongoDB 🎉");
+  });
+
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from Node.js backend!" });
+});
+
+// Example: API returning a list
+app.get("/api/users", (req, res) => {
+  res.json([
+    { id: 1, name: "Alice" },
+    { id: 2, name: "Bob" },
+  ]);
+});
 
 // Start server
 app.listen(PORT, () => {
